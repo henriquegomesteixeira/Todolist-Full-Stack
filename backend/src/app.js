f5a24c6f575ca9bfd6747887775d6cb9d2eefdb5
henriquegomesteixeira <henriquegomes080012@gmail.com>
2023-12-26 17:26:55 -0300

    Added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./router');
 
 const app = express();
 
 // Permite que o body da requisição seja convertido para JSON
 app.use(express.json());
+
+// Permite que o backend seja acessado por qualquer aplicação
+app.use(cors());
 
 // Define a rota raiz
 app.use(router);
